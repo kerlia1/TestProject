5d586292741c9fa521ebccaeeb342f4d2ed152a5
Kerlia_ <kerlia131@gmail.com>
2022-01-30 00:34:14 +0300

    Проверка
    Первичная проверка работы с гитом

---
+++ Assets/Player_Base.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Base : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("git работает?");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
